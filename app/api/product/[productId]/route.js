5c183c5a06620cee9f29c4469975c66661f7560e
Hasibul677 <hhasibul677@gmail.com>
2025-03-09 14:22:57 +0600

    create product admin backend

---
+++ app/api/product/[productId]/route.js
@@ -0,0 +1,73 @@
+import { NextResponse } from 'next/server';
+import connect from '@/utils/config/dbConnection';
+import { Product } from '@/utils/models/Product';
+
+export async function GET(req, { params }) {
+    await connect();
+
+    const { productId } = params;
+
+    try {
+        const product = await Product.findById(productId);
+        if (!product) {
+            return NextResponse.json({
+                error: "Product not found"
+            }, { status: 400 })
+        };
+
+        return NextResponse.json(product)
+    } catch (error) {
+        return NextResponse.json({
+            error: "Failed to fetching product"
+        }, { status: 500 })
+    }
+}
+
+export async function PUT(req, { params }) {
+
+    await connect();
+    const { productId } = params;
+    const body = await req.json();
+
+    try {
+        const updatedProduct = await Product.findByIdAndUpdate(productId, body, { new: true, runValidators: true });
+
+        if (!updatedProduct) {
+            return NextResponse.json({
+                error: "Product not found"
+            }, { status: 404 })
+        }
+
+        return NextResponse.json(updatedProduct)
+
+    } catch (error) {
+        return NextResponse.json({
+            error: "Error updating product"
+        }, { status: 500 })
+    }
+}
+
+export async function DELETE(req, { params }) {
+    await connect();
+    const { productId } = params;
+
+    try {
+        const deletedProduct = await Product.findByIdAndDelete(productId);
+
+        if (!deletedProduct) {
+            return NextResponse.json({
+                error: "Product not found"
+            }, { status: 404 });
+        }
+
+        return NextResponse.json({
+            message: "Product deleted successfully",
+            deletedProduct
+        });
+
+    } catch (error) {
+        return NextResponse.json({
+            error: "Error deleting product"
+        }, { status: 500 });
+    }
+}
---
+++ app/api/product/featured/[productId]/route.js
@@ -0,0 +1,40 @@
+import { NextResponse } from 'next/server';
+import connect from '@/utils/config/dbConnection';
+import { Product } from '@/utils/models/Product';
+
+export async function GET(req) {
+    await connect();
+
+    try {
+        const featuredProduct = await Product.findById({ featured: true });
+        return NextResponse.json(featuredProduct)
+    } catch (error) {
+        return NextResponse.json({
+            error: "Failed to fetching featured product"
+        }, { status: 500 })
+    }
+};
+
+export async function PUT(req, { params }) {
+
+    await connect();
+    const { productId } = params;
+
+    try {
+        const updatedFeaturedProducts = await Product.updateMany({ featured: true }, { featured: false });
+        const updateProduct = Product.findByIdAndUpdate(productId, { featured: true }, { new: true })
+
+        if (!updateProduct) {
+            return NextResponse.json({
+                error: "error finding featured product"
+            }, { status: 404 })
+        }
+
+        return NextResponse.json(updateProduct)
+
+    } catch (error) {
+        return NextResponse.json({
+            error: "Error updating product"
+        }, { status: 500 })
+    }
+}
