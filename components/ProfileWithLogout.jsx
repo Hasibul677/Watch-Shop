9283b0fc41ccde1dac8f55041c725c8569c66b3d
Hasibul677 <hhasibul677@gmail.com>
2025-05-04 10:08:57 +0600

    fix profile issues

--- components/ProfileWithLogout.jsx
+++ components/ProfileWithLogout.jsx
@@ -23,30 +23,31 @@ const ProfileWithLogout = () => {
     <div
       className="relative flex items-center pr-8 mt-2"
       onMouseEnter={() => setIsHovered(true)}
-      onMouseLeave={() => setIsHovered(false)}
     >
       {user?.name && <div className="text-center font-semibold mr-2">Profile</div>}
       <div className="relative">
         <Image
+          onMouseEnter={() => setIsHovered(true)}
           src={user?.image || "/profile.jpg"}
           width={40}
           height={40}
           className="rounded-full hover:border-2 border-emerald-500 transition-all duration-100"
           alt="Profile Picture"
-          onError={(e) => {
-            e.target.onerror = null;
-            e.target.src = "/profile.jpg";
-          }}
+          unoptimized
         />
-        {isHovered && user?.name && (
+        {(isHovered && user?.name) && (
           <div
+            onMouseLeave={() => setIsHovered(false)}
             className="absolute top-full right-0 mt-4 w-32 bg-white border border-gray-200 rounded-md shadow-lg z-10"
           >
-            <button className="w-full flex justify-center px-4 py-2 text-left hover:bg-gray-100">
+            <button
+              className="w-full flex justify-center px-4 py-2 text-left hover:bg-gray-100"
+            >
               <Link href="/profile">Profile</Link>
             </button>
             <Button
               variant="myButton"
+              onMouseEnter={() => setIsHovered(true)}
               className="w-full flex justify-center px-4 py-2 text-left"
               onClick={() => signOut()}
             >
